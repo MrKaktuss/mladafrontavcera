a11b53cce2f390950d8bda1092fdeaad3b0ff5a4
lopataa <49320136+lopataa@users.noreply.github.com>
2024-10-12 23:59:41 +0200

    seo improvements && sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs()]
+  site: "https://mladafrontavcera.cz",
+  integrations: [sitemap(), tailwind(), alpinejs()]
 });
